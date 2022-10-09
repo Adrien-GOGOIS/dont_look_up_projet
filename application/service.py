0d42878350071192285f8f9a84ebb938ee1b4149
Adrien GOGOIS <ad.gogois@gmail.com>
2022-10-09 10:00:57 +0200

    Basic display of names of asteroids in homepage

--- application/service.py
+++ application/service.py
@@ -1,19 +1,41 @@
 import requests
 
 
+class Asteroid():
+    asteroid_id = 0
+    name = ''
+    diameter = 0
+    close_approach_date = ''
+    miss_distance = ''
+
+    def __init__(self, asteroid_id, name, diameter, close_approach_date, miss_distance):
+        self.asteroid_id = asteroid_id
+        self.name = name
+        self.diameter = diameter
+        self.close_approach_date = close_approach_date
+        self.miss_distance = miss_distance
+
+
 def get_asteroids(start_date, end_date):
+    print('request...')
     url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=' \
-          + start_date \
+          + str(start_date) \
           + '&end_date=' \
-          + end_date \
+          + str(end_date) \
           + '&api_key=vQWT6RYdaTlM3uhavzlzima97RJeaKZgSMMkqf6D'
     response = requests.get(url)
     asteroids = response.json()
 
+    asteroids_list = []
+
     for date, asteroid in asteroids['near_earth_objects'].items():
-        print("Date : " + date)
-        print("ID : " + asteroid[0]['id'])
-        print("Name : " + asteroid[0]['name'])
+        asteroid_id = asteroid[0]['id']
+        name = asteroid[0]['name']
+        diameter = asteroid[0]['estimated_diameter']['meters']['estimated_diameter_max']
+        close_approach_date = asteroid[0]['close_approach_data'][0]['close_approach_date']
+        miss_distance = asteroid[0]['close_approach_data'][0]['miss_distance']['kilometers']
 
+        asteroid = Asteroid(asteroid_id, name, diameter, close_approach_date, miss_distance)
+        asteroids_list.append(asteroid)
 
-get_asteroids('2022-10-05', '2022-10-08')
+    return asteroids_list
--- application/views.py
+++ application/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from application.form import DateForm
+from application.service import get_asteroids
 
 
 def homepage(request):
-    form = DateForm()
+    asteroids = []
+
+    if request.method == 'POST':
+        form = DateForm(request.POST)
+        if form.is_valid():
+            start_date = form.cleaned_data['start_date']
+            end_date = form.cleaned_data['end_date']
+            asteroids = get_asteroids(start_date, end_date)
+    else:
+        form = DateForm()
 
     context = {
-        'form': form
+        'form': form,
+        'asteroids': asteroids
     }
 
     return render(request, 'application/homepage.html', context)
